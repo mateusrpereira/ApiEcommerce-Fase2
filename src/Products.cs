fc9f193c12556130a9cd90eca0269d13e58f6ed3
Mateus Ramos Pereira <mateusramospereira@hotmail.com>
2024-01-09 12:39:10 -0300

    Testando build

--- src/Products.cs
+++ src/Products.cs
@@ -22,7 +22,7 @@ namespace MyEcommerce.Products
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions! Deploy test");
+            response.WriteString("Welcome to Azure Functions! Deploy test trigger");
 
             return response;
         }
